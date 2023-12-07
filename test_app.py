a6421cc4981a22f1d6061773392db96585541f21
Efimov Alexey <efimov.alexey.a@gmail.com>
2023-12-07 18:21:23 +0500

    upd

--- test_app.py
+++ test_app.py
@@ -8,7 +8,25 @@ from  fastapi_ex import pe_urfu
 client = TestClient(pe_urfu)
 
 
-def test_read_main():
+def test_read_root():
     response = client.get("/")
     assert response.status_code == 200
-    
+
+
+def test_read_api():
+    response = client.get("/api")
+    assert response.status_code == 200
+
+def test_read_api():
+    response = client.get("/api")
+    assert response.status_code == 200
+
+def test_read_api_image_get():
+    response = client.get("/api/image/get")
+    assert response.status_code == 404
+
+
+def test_read_api_images():
+    response = client.get("/api/images")
+    assert response.status_code == 200
+
